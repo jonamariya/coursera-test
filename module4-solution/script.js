754ab7f491fbcc07d432a32c995f217d757effe5
Lenin Lawrence <Lenin Lawrence>
2016-12-22 10:51:28 +1030

    Assignment Solution for Module 4

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,11 +1,11 @@
 (function(){
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i=0;i<names.length;i++) {
-    var name=names[i];
-    if(name.charAt(0)==="j" || name.charAt(0)==="J" ){
-      byeSpeaker.speak(name);
-    }else{
-      helloSpeaker.speak(name);
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
     }
   }
 })();
